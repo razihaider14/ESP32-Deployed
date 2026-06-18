3fadf3059df37e330db8a01ed2f50acf6941384e
Razi Haider <therazi14@gmail.com>
2026-06-18 14:47:26 +0500

    First season random

--- weather.py
+++ weather.py
@@ -33,14 +33,15 @@ class WeatherSystem:
     def __init__(self):
         self.current = "Sunny"
         self.minutes_since_change = 0
-        self.season = random.choice(SEASON_NAMES)
+        self._start_season = random.choice(SEASON_NAMES)
+        self.season = self._start_season 
 
     def update(self, delta_minutes: float, current_hour: float = 12, current_day: int = 1):
         self.minutes_since_change += delta_minutes
         self._current_hour = current_hour
 
-        season_index = int((current_day - 1) / SEASON_LENGTH_DAYS) % 4
-        self.season = SEASON_NAMES[season_index]
+        start_index = SEASON_NAMES.index(self.season) if current_day == 1 else (SEASON_NAMES.index(self._start_season) + int((current_day - 1) / SEASON_LENGTH_DAYS)) % 4
+        self.season = SEASON_NAMES[start_index]
         
         if self.minutes_since_change >= WEATHER_CHANGE_INTERVAL:
             self.minutes_since_change = 0
